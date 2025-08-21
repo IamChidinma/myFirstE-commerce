98823788a13d4ba8b3d4300f0f4655828047c0a3
unknown <you@example.com>
2025-08-21 19:44:26 +0100

    Add project files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./Header.css";
+import { FaCartShopping, FaHeart } from "react-icons/fa6";
+import { FaSearch, FaUserCircle } from "react-icons/fa";
+import { RiMenuUnfold3Fill, RiMenuUnfold4Fill } from "react-icons/ri";
+import logo from "../assets/logo.png";
+const Header = () => {
+  const [showDRop, setShowDrop] = useState(false);
+  const [searchShow, setsearchShow] = useState(false);
+  const [profileShow, setprofileShow] = useState(false);
+  const [mobileShow, setmobileShow] = useState(false);
+
+  return (
+    <div className="Header_Body">
+      <header className="Header_wrapper">
+        <img src={logo} alt="Velvet Muse Logo" className="logo" />
+
+        <article className="right_Header_side">
+          <nav>
+            <ul>
+              <li>Name</li>
+              <li>About</li>
+              <li onMouseEnter={() => setShowDrop(true)}>show</li>
+            </ul>
+          </nav>
+          <div className="profile_with_icon">
+            <div className="icon_with_count">
+              <FaCartShopping className="icon" />
+              <div className="icon_count">0</div>
+            </div>
+            <div className="icon_with_count">
+              <FaHeart className="icon" />
+              <div className="icon_count">33</div>
+            </div>
+            <FaSearch
+              className="icon"
+              onClick={() => setsearchShow(!searchShow)}
+            />
+            <FaUserCircle
+              className="icon_profile"
+              onClick={() => setprofileShow(!profileShow)}
+            />
+            {!mobileShow ? (
+              <RiMenuUnfold3Fill
+                className="icon_menu_icon"
+                onClick={() => setmobileShow(true)}
+              />
+            ) : (
+              <RiMenuUnfold4Fill
+                className="icon_menu_icon"
+                onClick={() => setmobileShow(false)}
+              />
+            )}
+          </div>
+          {showDRop ? (
+            <div
+              className="Drop_show_body"
+              onMouseLeave={() => setShowDrop(false)}
+            ></div>
+          ) : null}
+        </article>
+        {mobileShow ? <div className="Mobile_nav"></div> : null}
+        {searchShow ? <div className="Search_body_show"></div> : null}
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  const images = [
+    "https://roarbeautystore.com/wp-content/uploads/2025/02/Adventure-Blend-Pro-Concealers.jpg.jpg.webp",
+    "https://roarbeautystore.com/wp-content/uploads/2025/08/Voan-Beauties-10-in-1-Fluffy-Mink-Lash-Set-Mix-01.jpg.webp",
+    "https://roarbeautystore.com/wp-content/uploads/2023/08/Adventure-Brow-Eyeliner-Gel.jpg.webp",
+    "https://roarbeautystore.com/wp-content/uploads/2024/03/Adventure-Luscious-18-in-1-Creamy-Lip-Culture-Palette.jpg.webp",
+    "https://roarbeautystore.com/wp-content/uploads/2024/02/Angel-Rose-10-Colour-Lipstick-Palette-Variants.jpg.webp",
+    "https://roarbeautystore.com/wp-content/uploads/2023/08/adventure-glasting-lip-gloss-1024x1024.jpg.webp",
+  ];
+
+  const [myTimer, setMyTimer] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMyTimer((oldValue) => (prev === images.length - 1 ? 0 : prev + 1));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <div className="Hero_body">
+      <div className="Hero_left"></div>
+      <div className="Hero_right">
+        <article className="product_container">
+          <img src={images[myTimer]} alt="makeup" className="img_con" />
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
